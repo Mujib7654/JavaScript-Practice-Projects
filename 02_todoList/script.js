786c941a205403fc6d9897c8697c42e75ae6c741
Mujibullah Inami <mujibullahinami26@gmail.com>
2023-11-05 23:56:17 +0530

    todo list

---
+++ 02_todoList/script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert('You must write something!');
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        //cross icon
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+};
+
+//checked and delete
+listContainer.addEventListener('click', function(e) {
+
+    if(e.target.tagName === 'LI' ){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+//save task in localstorage
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+//get data
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
